3fffb5d8016c8cb77487419a76cf46201cbcc1af
Ruslan Taghiyev <taghiyevr@itstep.local>
2018-11-18 17:58:01 +0400

    -

---
+++ resources/js/index.js
@@ -0,0 +1,15 @@
+// alert('Hello World');
+// function consoleLog() {
+//     console.log("TROLOLO");
+// }
+
+function consoleLog() {
+    console.log(document.getElementById('text').value);
+}
+
+function mult(number) {
+    return number*2;
+}
+
+let func = mult(2000);
+alert(func);
